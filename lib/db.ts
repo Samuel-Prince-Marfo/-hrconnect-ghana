fe176b2d9602f2013d5a1ba995496fc8dd399d30
Samuel-Prince-Marfo <130358493+Samuel-Prince-Marfo@users.noreply.github.com>
2026-07-21 03:16:03 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -8,7 +8,7 @@ const prismaClientSingleton = () => {
   return new PrismaClient({
     datasources: {
       db: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL || "postgresql://placeholder:placeholder@localhost:5432/placeholder",
       },
     },
   });
